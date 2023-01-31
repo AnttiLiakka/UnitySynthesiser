c6bd1a4f0d326dced670554224be7a6de4e8e06b
AnttiLiakka <108402924+AnttiLiakka@users.noreply.github.com>
2023-01-31 14:40:57 +0000

    Project Initialised

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,163 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+
+//==============================================================================
+UnitySynthesiserAudioProcessor::UnitySynthesiserAudioProcessor()
+     : AudioProcessor (BusesProperties()
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                       )
+{
+}
+
+UnitySynthesiserAudioProcessor::~UnitySynthesiserAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String UnitySynthesiserAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool UnitySynthesiserAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool UnitySynthesiserAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool UnitySynthesiserAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double UnitySynthesiserAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int UnitySynthesiserAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int UnitySynthesiserAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void UnitySynthesiserAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String UnitySynthesiserAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void UnitySynthesiserAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void UnitySynthesiserAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+
+}
+
+void UnitySynthesiserAudioProcessor::releaseResources()
+{
+
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool UnitySynthesiserAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void UnitySynthesiserAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+}
+
+//==============================================================================
+bool UnitySynthesiserAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* UnitySynthesiserAudioProcessor::createEditor()
+{
+    return new juce::GenericAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void UnitySynthesiserAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void UnitySynthesiserAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new UnitySynthesiserAudioProcessor();
+}
